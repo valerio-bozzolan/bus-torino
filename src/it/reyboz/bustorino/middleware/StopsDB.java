3b6a7cb635d7b85e0e03abb088efd03787350a33
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-05 16:09:14 +0100

    Get location.
    Since database now contains meaningful locations for nearly every stop, this might be useful...

--- src/it/reyboz/bustorino/middleware/StopsDB.java
+++ src/it/reyboz/bustorino/middleware/StopsDB.java
@@ -140,6 +140,35 @@ public class StopsDB extends SQLiteAssetHelper implements StopsDBInterface {
         return name;
     }
 
+    public String getLocationFromID(@NonNull String stopID) {
+        String[] uselessArray = {stopID};
+        int count;
+        String name;
+        Cursor result;
+
+        if(this.db == null) {
+            return null;
+        }
+
+        try {
+            result = this.db.query(QUERY_TABLE_stops, QUERY_COLUMN_location, QUERY_WHERE_ID, uselessArray, null, null, null);
+        } catch(SQLiteException e) {
+            return null;
+        }
+
+        count = result.getCount();
+        if(count == 0) {
+            return null;
+        }
+
+        result.moveToNext();
+        name = result.getString(0);
+
+        result.close();
+
+        return name;
+    }
+
     public Stop getAllFromID(@NonNull String stopID) {
         Cursor result;
         int count;
