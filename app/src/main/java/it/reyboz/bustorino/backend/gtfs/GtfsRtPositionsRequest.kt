3a5e18f2d23d4a5e244fb05cff7e882589fd5750
Fabio Mazza <fab4mazz@gmail.com>
2025-05-12 13:36:24 +0200

    Update libraries, add protocol buffer plugin
    Reviewers: valerio.bozzolan
    
    Reviewed By: valerio.bozzolan
    
    Subscribers: valerio.bozzolan, #libre_busto
    
    Tags: #libre_busto
    
    Differential Revision: https://gitpull.it/D190

--- app/src/main/java/it/reyboz/bustorino/backend/gtfs/GtfsRtPositionsRequest.kt
+++ app/src/main/java/it/reyboz/bustorino/backend/gtfs/GtfsRtPositionsRequest.kt
@@ -22,6 +22,7 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.HttpHeaderParser
+
 import com.google.transit.realtime.GtfsRealtime
 
 class GtfsRtPositionsRequest(
